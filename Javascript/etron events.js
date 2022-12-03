e3121dc805d004b569a5a6dbaeab0d80528c9c96
Sharif Hamdule <hamdulesharif555@gmail.com>
2022-12-03 20:00:32 +0530

    etron page

--- Javascript/etron events.js
+++ Javascript/etron events.js
@@ -78,3 +78,30 @@ function scrolle() {
         document.getElementById("d2").style.backgroundImage = "url('./Images/1.png')"
     }
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("img-carousel");
+    // let txt = document.getElementById("text-carousel");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
